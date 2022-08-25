bcc635cb7a0d4bc2b5fcb65c38773aa1665e40b3
m0ksem <m0ksem1337@gmail.com>
2022-08-25 11:58:18 +0000

    Deploying to gh-pages from @ m0ksem/new-portfolio@c6a99900b7ea9268a399dd8afbdc6729bd593548 🚀

---
+++ assets/use-fade-intersection.0094558f.js
@@ -0,0 +1,1 @@
+import{aO as n,a as o}from"./index.d604b8fa.js";const i=t=>{const s={value:!1};return n(t,([{isIntersecting:a}])=>{s.value=a;const e=o(t);a?(e.animate([{opacity:0},{opacity:1}],{duration:300}),e.style.opacity="1"):e.style.opacity="0"}),{isIntersecting:s}};export{i as u};
