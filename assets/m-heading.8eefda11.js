bcd057d947efcb0eeec7f068b4d4504b5ad9807b
m0ksem <m0ksem1337@gmail.com>
2023-01-24 18:26:16 +0000

    Deploying to gh-pages from @ m0ksem/new-portfolio@4375e7d1fc0f7726d9946765ad93cfdaab2180a0 🚀

---
+++ assets/m-heading.8eefda11.js
@@ -0,0 +1,1 @@
+import{_,b as a,e as n,aE as s}from"./index.24e8a0f8.js";const t={},r={class:"m-heading"};function c(e,o){return a(),n("h1",r,[s(e.$slots,"default",{},void 0,!0)])}var i=_(t,[["render",c],["__scopeId","data-v-9346c03c"]]);export{i as M};
