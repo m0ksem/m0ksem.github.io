7e316888a48e98a6bdf426605550cf6795853e95
m0ksem <m0ksem1337@gmail.com>
2023-04-17 14:12:51 +0000

    Deploying to gh-pages from @ m0ksem/m0ksem.github.io@b60510227c32fc03dcc46bbb6457aab1ce9d9113 🚀

---
+++ assets/m-heading.ef027d01.js
@@ -0,0 +1,1 @@
+import{_,b as a,e as n,aE as s}from"./index.aa1fff64.js";const t={},r={class:"m-heading"};function c(e,o){return a(),n("h1",r,[s(e.$slots,"default",{},void 0,!0)])}var i=_(t,[["render",c],["__scopeId","data-v-9346c03c"]]);export{i as M};
